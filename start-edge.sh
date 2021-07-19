ea9d5540e586d25a058fee27ac8138d69fd74911
climr <stinsonbj@ornl.gov>
2021-07-19 17:16:54 -0400

    Update start-edge.sh

--- start-edge.sh
+++ start-edge.sh
@@ -5,9 +5,10 @@ SUDO=$(test ${EUID} -ne 0 && which sudo)
 sleep 5
 #to do, make sure that video service is active first
 
-if systemctl is-active --quiet video ; then
-  echo "Video service is running, starting edge video stream" 
-  gst-client pipeline_play edge
-else
-  echo "Video service is not running, so not starting edge stream"
-fi
+#possible to start edge video stream now, but this may consume cell bandwdith even when no one is listening
+#if systemctl is-active --quiet video ; then
+#  echo "Video service is running, starting edge video stream" 
+#  gst-client pipeline_play edge
+#else
+#  echo "Video service is not running, so not starting edge stream"
+#fi
