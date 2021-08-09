2114330100b0d2d19aa17c40236e31dfd8b1e0cc
Brad Stinson <brad@horizon31.com>
2021-08-08 21:42:22 -0400

    cleanup

--- start-edge.sh
+++ start-edge.sh
@@ -2,13 +2,5 @@
 SUDO=$(test ${EUID} -ne 0 && which sudo)
 
 /usr/sbin/edge /etc/systemd/edge.conf
-sleep 5
-#to do, make sure that video service is active first
 
-if systemctl is-active --quiet video ; then
-  echo "Video service is running, starting edge video stream" 
-  gst-client pipeline_play edge
-  gst-client pipeline_play audio_edge
-else
-  echo "Video service is not running, so not starting edge stream"
-fi
+
